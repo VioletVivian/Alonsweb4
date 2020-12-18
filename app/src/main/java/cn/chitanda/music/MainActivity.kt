c2e98a0abb0f11bf57757317a77cf64914445e1d
VioletVivian <100173231+VioletVivian@users.noreply.github.com>
2020-12-18 20:53:26 +0000

    * Update German

---
+++ app/src/main/java/cn/chitanda/music/MainActivity.kt
@@ -0,0 +1,9 @@
+package cn.chitanda.music
+
+import android.os.Build
+import android.os.Bundle
+import android.view.ViewTreeObserver
+import androidx.activity.compose.setContent
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.anima
