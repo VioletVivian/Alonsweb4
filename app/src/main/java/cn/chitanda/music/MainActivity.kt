22c3e0deb32b1900d57c0b9838361a578c40f0c3
VioletVivian <100173231+VioletVivian@users.noreply.github.com>
2020-12-19 09:11:17 +0000

    Found AU.

--- app/src/main/java/cn/chitanda/music/MainActivity.kt
+++ app/src/main/java/cn/chitanda/music/MainActivity.kt
@@ -6,4 +6,8 @@ import android.view.ViewTreeObserver
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.anima
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.CompositionLocalProvider
