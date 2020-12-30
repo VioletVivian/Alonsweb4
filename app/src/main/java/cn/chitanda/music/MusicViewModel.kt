82b79b8cd0f63e0470efaebf36dc646636560668
VioletVivian <100173231+VioletVivian@users.noreply.github.com>
2020-12-30 19:46:50 +0000

    Linux 3.0.51

--- app/src/main/java/cn/chitanda/music/MusicViewModel.kt
+++ app/src/main/java/cn/chitanda/music/MusicViewModel.kt
@@ -1,4 +1,11 @@
 package cn.chitanda.music
 
 import android.support.v4.media.MediaBrowserCompat
-import android.support.v4.media.MediaM
+import android.support.v4.media.MediaMetadataCompat
+import android.support.v4.media.session.PlaybackStateCompat
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import cn.chitanda.music.media
