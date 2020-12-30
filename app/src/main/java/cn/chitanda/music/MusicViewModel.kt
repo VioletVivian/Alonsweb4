fbe448e89bc122c2a08899aa8189fb549c66a645
VioletVivian <100173231+VioletVivian@users.noreply.github.com>
2020-12-30 20:37:32 +0000

    add todo

--- app/src/main/java/cn/chitanda/music/MusicViewModel.kt
+++ app/src/main/java/cn/chitanda/music/MusicViewModel.kt
@@ -8,4 +8,9 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import cn.chitanda.music.media
+import cn.chitanda.music.media.connect.MusicServiceConnection
+import cn.chitanda.music.media.extensions.currentPlayBackPosition
+import cn.chitanda.music.media.extensions.duration
+import cn.chitanda.music.media.extensions.id
+import cn.chitanda.music.media.extensions.isPlayEnabled
+import cn.chita
