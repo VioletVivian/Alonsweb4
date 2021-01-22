f32b69ea9a26f0b0c5539d0130468607fe586c6b
VioletVivian <100173231+VioletVivian@users.noreply.github.com>
2021-01-22 14:43:20 +0000

    Add roadmap

--- app/src/main/java/cn/chitanda/music/di/AppModule.kt
+++ app/src/main/java/cn/chitanda/music/di/AppModule.kt
@@ -6,4 +6,11 @@ import cn.chitanda.music.BuildConfig
 import cn.chitanda.music.http.MyCookieJar
 import cn.chitanda.music.http.api.*
 import cn.chitanda.music.http.moshi.moshi
+import cn.chitanda.music.preference.CookiesPreference
+import coil.imageLoader
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
 import
