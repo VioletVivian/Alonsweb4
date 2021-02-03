d5a570dfe994c0cae23ad74c5008814b6a9f08d0
VioletVivian <100173231+VioletVivian@users.noreply.github.com>
2021-02-03 23:05:40 +0000

    my change

--- app/src/main/java/cn/chitanda/music/di/MediaModule.kt
+++ app/src/main/java/cn/chitanda/music/di/MediaModule.kt
@@ -6,3 +6,10 @@ import cn.chitanda.music.media.MusicService
 import cn.chitanda.music.media.MusicSource
 import cn.chitanda.music.media.MusicSourceImp
 import cn.chitanda.music.media.connect.MusicServiceConnection
+import cn.chitanda.music.repository.SongsRepository
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.EntryPoint
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.Sin
