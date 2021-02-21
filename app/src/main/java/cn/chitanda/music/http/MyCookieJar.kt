8f15d070100b2c5e9457293cdc0178388d14c890
VioletVivian <100173231+VioletVivian@users.noreply.github.com>
2021-02-21 07:54:28 +0000

    add echo

--- app/src/main/java/cn/chitanda/music/http/MyCookieJar.kt
+++ app/src/main/java/cn/chitanda/music/http/MyCookieJar.kt
@@ -1,3 +1,11 @@
 package cn.chitanda.music.http
 
-import cn.chit
+import cn.chitanda.music.http.moshi.moshi
+import cn.chitanda.music.preference.CookiesPreference
+import okhttp3.Cookie
+import okhttp3.CookieJar
+import okhttp3.HttpUrl
+
+private const val TAG = "MyCookieJar"
+
+class MyCookieJar(cookiesPreference: CookiesPreference) : Cook
