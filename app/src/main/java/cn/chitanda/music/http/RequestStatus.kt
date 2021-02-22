8f524ccd88620ccb5622cc46e0d66384341ced90
VioletVivian <100173231+VioletVivian@users.noreply.github.com>
2021-02-22 06:38:03 +0000

    More simplification

--- app/src/main/java/cn/chitanda/music/http/RequestStatus.kt
+++ app/src/main/java/cn/chitanda/music/http/RequestStatus.kt
@@ -1,1 +1,14 @@
-package cn
+package cn.chitanda.music.http
+
+import cn.chitanda.music.http.bean.BaseJson
+import com.squareup.moshi.JsonClass
+
+/**
+ *@author: Chen
+ *@createTime: 2021/8/31 13:45
+ *@description:
+ **/
+@JsonClass(generateAdapter = true)
+data class RequestStatus<T : BaseJson<*>>(
+    var code: Int = 0,
+    var 
