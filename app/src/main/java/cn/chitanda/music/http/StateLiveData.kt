5ddfc84bb0874b0492a97db27c8cce817b03cb7a
VioletVivian <100173231+VioletVivian@users.noreply.github.com>
2021-02-22 22:21:36 +0000

    Update the docs.

--- app/src/main/java/cn/chitanda/music/http/StateLiveData.kt
+++ app/src/main/java/cn/chitanda/music/http/StateLiveData.kt
@@ -1,4 +1,10 @@
 package cn.chitanda.music.http
 
 import androidx.lifecycle.MutableLiveData
-impo
+import androidx.lifecycle.Observer
+import cn.chitanda.music.http.bean.BaseJson
+
+class StateLiveData<T:BaseJson<*>> : MutableLiveData<RequestStatus<T>>(RequestStatus())
+
+abstract class IStateObserver<T:BaseJson<*>> : Observer<RequestStatus<T>> {
+    override fun
