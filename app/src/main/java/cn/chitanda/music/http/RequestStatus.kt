5b89b1eb1c4f907ee55e0ed01d6289702b404795
VioletVivian <100173231+VioletVivian@users.noreply.github.com>
2021-02-22 09:15:44 +0000

    Adjust version

--- app/src/main/java/cn/chitanda/music/http/RequestStatus.kt
+++ app/src/main/java/cn/chitanda/music/http/RequestStatus.kt
@@ -11,4 +11,15 @@ import com.squareup.moshi.JsonClass
 @JsonClass(generateAdapter = true)
 data class RequestStatus<T : BaseJson<*>>(
     var code: Int = 0,
-    var 
+    var error: Throwable? = null,
+    var msg: String? = null,
+    var status: DataState = DataState.STATE_CREATE,
+    var json: T? = null,
+) {
+    val data get() = json?.data
+}
+
+enum class DataState {
+    STATE_CREATE,//创建
+    STATE_LOADING,//加载中
+    STATE_SUCCESS,//
