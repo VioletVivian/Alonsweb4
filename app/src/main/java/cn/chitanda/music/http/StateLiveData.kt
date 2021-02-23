bc04b4f34fcd31c275f1e6ff05460ac10264d695
VioletVivian <100173231+VioletVivian@users.noreply.github.com>
2021-02-23 07:39:19 +0000

    configs

--- app/src/main/java/cn/chitanda/music/http/StateLiveData.kt
+++ app/src/main/java/cn/chitanda/music/http/StateLiveData.kt
@@ -7,4 +7,14 @@ import cn.chitanda.music.http.bean.BaseJson
 class StateLiveData<T:BaseJson<*>> : MutableLiveData<RequestStatus<T>>(RequestStatus())
 
 abstract class IStateObserver<T:BaseJson<*>> : Observer<RequestStatus<T>> {
-    override fun
+    override fun onChanged(t: RequestStatus<T>) {
+        when (t.status) {
+            DataState.STATE_SUCCESS -> {
+                //请求成功，数据不为null
+                t.json?.let {
+                    onDataChange(it)
+                }
+            }
+
+            DataState.STATE_EMPTY -> {
+        
