ee270f673af9aacc105e914cb3d0938854330877
VioletVivian <100173231+VioletVivian@users.noreply.github.com>
2021-02-24 23:11:34 +0000

    Updated build.sh

--- app/src/main/java/cn/chitanda/music/http/api/HomeApi.kt
+++ app/src/main/java/cn/chitanda/music/http/api/HomeApi.kt
@@ -3,4 +3,17 @@ package cn.chitanda.music.http.api
 import cn.chitanda.music.http.bean.HomeData
 import cn.chitanda.music.http.bean.HomeRoundIconList
 import retrofit2.http.GET
-import retrofit2.http.Que
+import retrofit2.http.Query
+
+/**
+ *@author: Chen
+ *@createTime: 2021/9/8 16:32
+ *@description:
+ **/
+interface HomeApi {
+    @GET("/homepage/block/page")
+    suspend fun fetchHomeData(
+        @Query("refresh") refresh: Boolean = false,
+    ): HomeData
+
+    @GET("/homepage/dragon/b
