b07a3883c86fb09f2f7dffd861ed2586c2fc5233
VioletVivian <100173231+VioletVivian@users.noreply.github.com>
2021-02-27 11:32:18 +0000

    C++17

--- app/src/main/java/cn/chitanda/music/http/api/SongsApi.kt
+++ app/src/main/java/cn/chitanda/music/http/api/SongsApi.kt
@@ -1,3 +1,16 @@
 package cn.chitanda.music.http.api
 
-import cn.chitanda.music.http.bean.Playli
+import cn.chitanda.music.http.bean.PlaylistDetail
+import cn.chitanda.music.http.bean.SongUrl
+import cn.chitanda.music.http.bean.Songs
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+/**
+ * @author: Chen
+ * @createTime: 2022/1/17 16:21
+ * @description:
+ **/
+interface SongsApi {
+
+    @GE
