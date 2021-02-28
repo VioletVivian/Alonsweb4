47f1da000198260068b7b491fe5c8eaca3a4b9ed
VioletVivian <100173231+VioletVivian@users.noreply.github.com>
2021-02-28 13:59:15 +0000

    update migrations

--- app/src/main/java/cn/chitanda/music/http/api/VideoApi.kt
+++ app/src/main/java/cn/chitanda/music/http/api/VideoApi.kt
@@ -2,4 +2,16 @@ package cn.chitanda.music.http.api
 
 import cn.chitanda.music.http.bean.VideoList
 import cn.chitanda.music.http.bean.VideoType
-import retrofit2.h
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface VideoApi {
+
+    @GET("/video/group/list")
+    suspend fun getVideoType(): VideoType
+
+    @GET("/video/group")
+    suspend fun getVideoByType(
+        @Query("id") typeId: Int,
+        @Query("offset") offset: Int
+    
