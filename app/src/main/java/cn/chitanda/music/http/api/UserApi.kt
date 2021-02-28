df38f739300795d9eac838b8d76a7c57e6a1bde0
VioletVivian <100173231+VioletVivian@users.noreply.github.com>
2021-02-28 06:23:58 +0000

    First post!

--- app/src/main/java/cn/chitanda/music/http/api/UserApi.kt
+++ app/src/main/java/cn/chitanda/music/http/api/UserApi.kt
@@ -1,1 +1,12 @@
-package cn.chitanda.music.htt
+package cn.chitanda.music.http.api
+
+import cn.chitanda.music.http.bean.PlaylistJson
+import cn.chitanda.music.http.bean.RefreshLogin
+import cn.chitanda.music.http.bean.UserProfile
+import retrofit2.http.GET
+import retrofit2.http.POST
+import retrofit2.http.Query
+
+/**
+ *@author: Chen
+ *@createTime: 2021/8/31 1
