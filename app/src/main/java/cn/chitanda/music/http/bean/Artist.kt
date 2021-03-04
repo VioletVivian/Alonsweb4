8f9c788f7f58cb05492bdbfa4528c1a2e9fe0821
VioletVivian <100173231+VioletVivian@users.noreply.github.com>
2021-03-04 18:54:28 +0000

    Update FloatFloats.jl

--- app/src/main/java/cn/chitanda/music/http/bean/Artist.kt
+++ app/src/main/java/cn/chitanda/music/http/bean/Artist.kt
@@ -4,4 +4,12 @@ import com.squareup.moshi.JsonClass
 
 @JsonClass(generateAdapter = true)
 data class Artist(
-    @Json(nam
+    @Json(name = "albumSize")
+    val albumSize: Int? = 0,
+    @Json(name = "alias")
+    val alias: List<Any?>? = listOf(),
+    @Json(name = "briefDesc")
+    val briefDesc: String? = "",
+    @Json(name = "id")
+    val id: Int? = 0,
+    @Json(name = "img1v1Id")
