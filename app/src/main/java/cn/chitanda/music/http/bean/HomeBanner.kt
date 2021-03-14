c89dd074c11c2151e2bb87ee32bd0870f183eac2
VioletVivian <100173231+VioletVivian@users.noreply.github.com>
2021-03-14 07:00:24 +0000

    Fix travis error.

--- app/src/main/java/cn/chitanda/music/http/bean/HomeBanner.kt
+++ app/src/main/java/cn/chitanda/music/http/bean/HomeBanner.kt
@@ -5,4 +5,13 @@ import androidx.compose.ui.graphics.Color
 import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
 
-@JsonClass(generateAdapt
+@JsonClass(generateAdapter = true)
+data class HomeBanner(
+    @Json(name = "banners")
+    val banners: List<Banner>? = listOf(),
+) {
+    @JsonClass(generateAdapter = true)
+    data class Banner(
+        @Json(name = "adDispatchJson")
+        val adDispatchJson: Any? = Any(),
+        @Json(name 
