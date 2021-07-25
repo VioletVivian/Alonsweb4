5814b0206bb0446028d273f60b66840dab6fcb8f
VioletVivian <100173231+VioletVivian@users.noreply.github.com>
2021-07-25 09:27:53 +0000

    Fix #18

--- app/src/main/java/cn/chitanda/music/http/bean/HomeData.kt
+++ app/src/main/java/cn/chitanda/music/http/bean/HomeData.kt
@@ -242,3 +242,257 @@ data class HomeData(
                                 val copyrightId: Long? = 0,
                                 @Json(name = "description")
                                 val description: String? = "",
+                                @Json(name = "id")
+                                val id: Long? = 0,
+                                @Json(name = "mark")
+                                val mark: Long? = 0,
+                                @Json(name = "name")
+                                val name: String? = "",
+                                @Json(name = "onSale")
+                                val onSale: Boolean? = false,
+                                @Json(name = "pic")
+                                val pic: Long? = 0,
+                                @Json(name = "picId")
+                                val picId: Long? = 0,
+                                @Json(name = "picId_str")
+                                val picIdStr: String? = "",
+                                @Json(name = "picUrl")
+                                val picUrl: String? = "",
+                                @Json(name = "publishTime")
+                                val publishTime: Long? = 0,
+                                @Json(name = "size")
+                                val size: Long? = 0,
+                                @Json(name = "songs")
+                                val songs: List<Any?>? = listOf(),
+                                @Json(name = "status")
+                                val status: Long? = 0,
+                                @Json(name = "subType")
+                                val subType: String? = "",
+                                @Json(name = "tags")
+                                val tags: String? = "",
+                                @Json(name = "transName")
+                                val transName: Any? = Any(),
+                                @Json(name = "type")
+                                val type: String? = ""
+                            )
+
+                            @JsonClass(generateAdapter = true)
+                            data class BMusic(
+                                @Json(name = "bitrate")
+                                val bitrate: Long? = 0,
+                                @Json(name = "dfsId")
+                                val dfsId: Long? = 0,
+                                @Json(name = "extension")
+                                val extension: String? = "",
+                                @Json(name = "id")
+                                val id: Long? = 0,
+                                @Json(name = "name")
+                                val name: Any? = Any(),
+                                @Json(name = "playTime")
+                                val playTime: Long? = 0,
+                                @Json(name = "size")
+                                val size: Long? = 0,
+                                @Json(name = "sr")
+                                val sr: Long? = 0,
+                                @Json(name = "volumeDelta")
+                                val volumeDelta: Any? = Any()
+                            )
+
+                            @JsonClass(generateAdapter = true)
+                            data class LMusic(
+                                @Json(name = "bitrate")
+                                val bitrate: Long? = 0,
+                                @Json(name = "dfsId")
+                                val dfsId: Long? = 0,
+                                @Json(name = "extension")
+                                val extension: String? = "",
+                                @Json(name = "id")
+                                val id: Long? = 0,
+                                @Json(name = "name")
+                                val name: Any? = Any(),
+                                @Json(name = "playTime")
+                                val playTime: Long? = 0,
+                                @Json(name = "size")
+                                val size: Long? = 0,
+                                @Json(name = "sr")
+                                val sr: Long? = 0,
+                                @Json(name = "volumeDelta")
+                                val volumeDelta: Any? = Any()
+                            )
+
+                            @JsonClass(generateAdapter = true)
+                            data class MMusic(
+                                @Json(name = "bitrate")
+                                val bitrate: Long? = 0,
+                                @Json(name = "dfsId")
+                                val dfsId: Long? = 0,
+                                @Json(name = "extension")
+                                val extension: String? = "",
+                                @Json(name = "id")
+                                val id: Long? = 0,
+                                @Json(name = "name")
+                                val name: Any? = Any(),
+                                @Json(name = "playTime")
+                                val playTime: Long? = 0,
+                                @Json(name = "size")
+                                val size: Long? = 0,
+                                @Json(name = "sr")
+                                val sr: Long? = 0,
+                                @Json(name = "volumeDelta")
+                                val volumeDelta: Any? = Any()
+                            )
+                        }
+
+                        @JsonClass(generateAdapter = true)
+                        data class SongPrivilege(
+                            @Json(name = "chargeInfoList")
+                            val chargeInfoList: List<ChargeInfo?>? = listOf(),
+                            @Json(name = "cp")
+                            val cp: Long? = 0,
+                            @Json(name = "cs")
+                            val cs: Boolean? = false,
+                            @Json(name = "dl")
+                            val dl: Long? = 0,
+                            @Json(name = "downloadMaxbr")
+                            val downloadMaxbr: Long? = 0,
+                            @Json(name = "fee")
+                            val fee: Long? = 0,
+                            @Json(name = "fl")
+                            val fl: Long? = 0,
+                            @Json(name = "flag")
+                            val flag: Long? = 0,
+                            @Json(name = "freeTrialPrivilege")
+                            val freeTrialPrivilege: FreeTrialPrivilege? = FreeTrialPrivilege(),
+                            @Json(name = "id")
+                            val id: Long? = 0,
+                            @Json(name = "maxbr")
+                            val maxbr: Long? = 0,
+                            @Json(name = "paidBigBang")
+                            val paidBigBang: Boolean? = false,
+                            @Json(name = "payed")
+                            val payed: Long? = 0,
+                            @Json(name = "pc")
+                            val pc: Any? = Any(),
+                            @Json(name = "pl")
+                            val pl: Long? = 0,
+                            @Json(name = "playMaxbr")
+                            val playMaxbr: Long? = 0,
+                            @Json(name = "preSell")
+                            val preSell: Boolean? = false,
+                            @Json(name = "realPayed")
+                            val realPayed: Long? = 0,
+                            @Json(name = "rscl")
+                            val rscl: Any? = Any(),
+                            @Json(name = "sp")
+                            val sp: Long? = 0,
+                            @Json(name = "st")
+                            val st: Long? = 0,
+                            @Json(name = "subp")
+                            val subp: Long? = 0,
+                            @Json(name = "toast")
+                            val toast: Boolean? = false
+                        ) {
+                            @JsonClass(generateAdapter = true)
+                            data class ChargeInfo(
+                                @Json(name = "chargeMessage")
+                                val chargeMessage: Any? = Any(),
+                                @Json(name = "chargeType")
+                                val chargeType: Long? = 0,
+                                @Json(name = "chargeUrl")
+                                val chargeUrl: Any? = Any(),
+                                @Json(name = "rate")
+                                val rate: Long? = 0
+                            )
+
+                            @JsonClass(generateAdapter = true)
+                            data class FreeTrialPrivilege(
+                                @Json(name = "resConsumable")
+                                val resConsumable: Boolean? = false,
+                                @Json(name = "userConsumable")
+                                val userConsumable: Boolean? = false
+                            )
+                        }
+
+                        @JsonClass(generateAdapter = true)
+                        data class User(
+                            @Json(name = "accountStatus")
+                            val accountStatus: Long? = 0,
+                            @Json(name = "authStatus")
+                            val authStatus: Long? = 0,
+                            @Json(name = "authority")
+                            val authority: Long? = 0,
+                            @Json(name = "avatarImgId")
+                            val avatarImgId: Long? = 0,
+                            @Json(name = "avatarImgIdStr")
+                            val avatarImgIdStr: String? = "",
+                            @Json(name = "avatarUrl")
+                            val avatarUrl: String? = "",
+                            @Json(name = "backgroundImgId")
+                            val backgroundImgId: Long? = 0,
+                            @Json(name = "backgroundImgIdStr")
+                            val backgroundImgIdStr: String? = "",
+                            @Json(name = "backgroundUrl")
+                            val backgroundUrl: Any? = Any(),
+                            @Json(name = "birthday")
+                            val birthday: Long? = 0,
+                            @Json(name = "city")
+                            val city: Long? = 0,
+                            @Json(name = "defaultAvatar")
+                            val defaultAvatar: Boolean? = false,
+                            @Json(name = "description")
+                            val description: Any? = Any(),
+                            @Json(name = "detailDescription")
+                            val detailDescription: Any? = Any(),
+                            @Json(name = "djStatus")
+                            val djStatus: Long? = 0,
+                            @Json(name = "expertTags")
+                            val expertTags: Any? = Any(),
+                            @Json(name = "experts")
+                            val experts: Any? = Any(),
+                            @Json(name = "extProperties")
+                            val extProperties: Any? = Any(),
+                            @Json(name = "followed")
+                            val followed: Boolean? = false,
+                            @Json(name = "gender")
+                            val gender: Long? = 0,
+                            @Json(name = "mutual")
+                            val mutual: Boolean? = false,
+                            @Json(name = "nickname")
+                            val nickname: String? = "",
+                            @Json(name = "province")
+                            val province: Long? = 0,
+                            @Json(name = "remarkName")
+                            val remarkName: Any? = Any(),
+                            @Json(name = "signature")
+                            val signature: Any? = Any(),
+                            @Json(name = "userId")
+                            val userId: Long? = 0,
+                            @Json(name = "userType")
+                            val userType: Long? = 0,
+                            @Json(name = "vipType")
+                            val vipType: Long? = 0,
+                            @Json(name = "xInfo")
+                            val xInfo: Any? = Any()
+                        )
+                    }
+
+                    @JsonClass(generateAdapter = true)
+                    data class UiElement(
+                        @Json(name = "image")
+                        val image: Image? = Image(),
+                        @Json(name = "labelTexts")
+                        val labelTexts: List<String?>? = listOf(),
+                        @Json(name = "mainTitle")
+                        val mainTitle: MainTitle? = MainTitle(),
+                        @Json(name = "rcmdShowType")
+                        val rcmdShowType: String? = "",
+                        @Json(name = "subTitle")
+                        val subTitle: SubTitle? = SubTitle()
+                    ) {
+                        @JsonClass(generateAdapter = true)
+                        data class Image(
+                            @Json(name = "imageUrl")
+                            val imageUrl: String? = ""
+                        )
+
+                        @JsonClass(generateAdapter = true)
