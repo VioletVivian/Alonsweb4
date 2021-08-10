44a3da26aef3b2b6155fa65fcb37a1d58d1a46b2
VioletVivian <100173231+VioletVivian@users.noreply.github.com>
2021-08-10 10:19:03 +0000

    some more translations

---
+++ app/src/main/java/cn/chitanda/music/http/bean/RefreshLogin.kt
@@ -0,0 +1,8 @@
+package cn.chitanda.music.http.bean
+
+data class RefreshLogin(
+    override val code: Int,
+    override val msg: String?,
+    override val message: String?,
+    override val data: Nothing
+) : BaseJson<N
