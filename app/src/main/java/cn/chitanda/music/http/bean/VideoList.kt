70a22f0e43b6bf517ce18ccbf9cf14ee82f2060e
VioletVivian <100173231+VioletVivian@users.noreply.github.com>
2021-09-26 20:42:55 +0000

    sync en

--- app/src/main/java/cn/chitanda/music/http/bean/VideoList.kt
+++ app/src/main/java/cn/chitanda/music/http/bean/VideoList.kt
@@ -3,4 +3,12 @@ package cn.chitanda.music.http.bean
 
 import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
-@JsonClass(generateAdapte
+@JsonClass(generateAdapter = true)
+data class VideoList(
+    override val code: Int,
+    override val message: String?,
+    override val msg: String?, @Json(name = "hasmore")
+    val hasMore: Boolean,
+    @Json(name = "datas")
+    override val data: List<Data>?
+) : BaseJson<List<V
