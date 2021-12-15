6925918f8e1713b0f8ad5f717bd4f737139e5a20
VioletVivian <100173231+VioletVivian@users.noreply.github.com>
2021-12-15 07:31:37 +0000

    new rule

--- app/src/main/java/cn/chitanda/music/repository/BaseRemoteRepository.kt
+++ app/src/main/java/cn/chitanda/music/repository/BaseRemoteRepository.kt
@@ -3,4 +3,15 @@ package cn.chitanda.music.repository
 import cn.chitanda.music.http.DataState
 import cn.chitanda.music.http.RequestStatus
 import cn.chitanda.music.http.bean.BaseJson
-import kotlinx.coroutines
+import kotlinx.coroutines.flow.MutableStateFlow
+
+/**
+ *@author: Chen
+ *@createTime: 2021/8/31 15:05
+ *@description:
+ **/
+open class BaseRemoteRepository {
+
+    protected suspend fun <T : BaseJson<*>> httpRequest(
+        stateFlow: MutableStateFlow<RequestStatus<T>>? = null,
+ 
