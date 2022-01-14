044f5aef32420739b85d27111581869cc6822730
VioletVivian <100173231+VioletVivian@users.noreply.github.com>
2022-01-14 13:47:58 +0000

    Beta 1

--- app/src/main/java/cn/chitanda/music/repository/SongsRepository.kt
+++ app/src/main/java/cn/chitanda/music/repository/SongsRepository.kt
@@ -3,3 +3,12 @@ package cn.chitanda.music.repository
 import cn.chitanda.music.http.api.SongsApi
 import cn.chitanda.music.ui.scene.playlist.PlaylistViewState
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+
+/**
+ * @author: Chen
+ * @createTime: 2022/1/17 16:21
+ * @description:
+ **/
+class SongsRepository(private val api: SongsApi) : BaseRemoteRepository() {
+    suspend fun getPlaylistDetail(id: String) = withContext(Dispatchers.
