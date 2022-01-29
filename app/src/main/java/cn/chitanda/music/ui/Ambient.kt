84da4447e09447433d7b56297610c776fa622593
VioletVivian <100173231+VioletVivian@users.noreply.github.com>
2022-01-29 08:18:21 +0000

    Create s

--- app/src/main/java/cn/chitanda/music/ui/Ambient.kt
+++ app/src/main/java/cn/chitanda/music/ui/Ambient.kt
@@ -1,4 +1,12 @@
 package cn.chitanda.music.ui
 
 import androidx.compose.runtime.compositionLocalOf
-import androidx.compose.runtime
+import androidx.compose.runtime.staticCompositionLocalOf
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavHostController
+import cn.chitanda.music.MusicViewModel
+import cn.chitanda.music.ui.scene.LocaleUserViewModel
+import cn.chitanda.music.ui.scene.ThemeViewModel
+
+/**
+ *@
