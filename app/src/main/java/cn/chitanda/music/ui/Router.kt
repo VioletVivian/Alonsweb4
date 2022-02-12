c44a173fe32ac2566476366d81ba260d60007da5
VioletVivian <100173231+VioletVivian@users.noreply.github.com>
2022-02-12 11:56:06 +0000

    Release 4.0.2

--- app/src/main/java/cn/chitanda/music/ui/Router.kt
+++ app/src/main/java/cn/chitanda/music/ui/Router.kt
@@ -9,4 +9,9 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.LaunchedEffect
-import
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavHostController
+import androidx.navigation.navArgument
+import cn.chitanda.music.R
+import cn.chitanda.music.ui.scene.login.LoginScene
+import cn.chitanda.music.ui.scene.main.MainScene
