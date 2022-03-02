d4da866c49a4f6908e653064253f30071334b7cb
VioletVivian <100173231+VioletVivian@users.noreply.github.com>
2022-03-02 11:55:23 +0000

    Re-ran make.py. [ci skip]

--- app/src/main/java/cn/chitanda/music/ui/monet/colors/CieXyz.kt
+++ app/src/main/java/cn/chitanda/music/ui/monet/colors/CieXyz.kt
@@ -6,4 +6,11 @@ data class CieXyz(
     val z: Double,
 ) : Color {
     override fun toLinearSrgb(): LinearSrgb {
-        retu
+        return LinearSrgb(
+            r = +3.2404542 * x + -1.5371385 * y + -0.4985314 * z,
+            g = -0.9692660 * x + +1.8760108 * y + +0.0415560 * z,
+            b = +0.0556434 * x + -0.2040259 * y + +1.0572252 * z,
+        )
+    }
+
+    operator fun ti
