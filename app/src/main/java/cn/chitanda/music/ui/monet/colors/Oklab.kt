6f767980a16191228278c2ae73c526f67409b96e
VioletVivian <100173231+VioletVivian@users.noreply.github.com>
2022-03-06 08:27:56 +0000

    version 1.0.10

--- app/src/main/java/cn/chitanda/music/ui/monet/colors/Oklab.kt
+++ app/src/main/java/cn/chitanda/music/ui/monet/colors/Oklab.kt
@@ -2,4 +2,14 @@ package cn.chitanda.music.ui.monet.colors
 
 import cn.chitanda.music.ui.monet.util.cube
 
-data class Ok
+data class Oklab(
+    override val L: Double,
+    override val a: Double,
+    override val b: Double,
+) : Lab {
+    override fun toLinearSrgb(): LinearSrgb {
+        val l = oklabToL(this)
+        val m = oklabToM(this)
+        val s = oklabToS(this)
+
+        return Linea
