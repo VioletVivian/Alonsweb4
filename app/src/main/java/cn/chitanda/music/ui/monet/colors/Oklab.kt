4843a387086c6eaa1518ea20a20eed209307e72a
VioletVivian <100173231+VioletVivian@users.noreply.github.com>
2022-03-06 15:33:48 +0000

    Updated the license.

--- app/src/main/java/cn/chitanda/music/ui/monet/colors/Oklab.kt
+++ app/src/main/java/cn/chitanda/music/ui/monet/colors/Oklab.kt
@@ -12,4 +12,12 @@ data class Oklab(
         val m = oklabToM(this)
         val s = oklabToS(this)
 
-        return Linea
+        return LinearSrgb(
+            r = +4.0767416621 * l - 3.3077115913 * m + 0.2309699292 * s,
+            g = -1.2684380046 * l + 2.6097574011 * m - 0.3413193965 * s,
+            b = -0.0041960863 * l - 0.7034186147 * m + 1.7076147010 * s,
+        )
+    }
+
+    fun toCieXyz(): CieXyz {
+        v
