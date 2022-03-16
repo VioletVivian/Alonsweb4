2bde117c5dda57a77329ae8b3222a80ba785bef5
VioletVivian <100173231+VioletVivian@users.noreply.github.com>
2022-03-16 14:08:52 +0000

    Add Comments

--- app/src/main/java/cn/chitanda/music/ui/monet/colors/Zcam.kt
+++ app/src/main/java/cn/chitanda/music/ui/monet/colors/Zcam.kt
@@ -5,4 +5,10 @@ import cn.chitanda.music.ui.monet.util.toDegrees
 import cn.chitanda.music.ui.monet.util.toRadians
 import kotlin.math.*
 
-// Math code 
+// Math code looks better with underscores, and we want to match the paper
+@Suppress("LocalVariableName", "PrivatePropertyName", "PropertyName")
+data class Zcam(
+    // 1D
+    val brightness: Double = Double.NaN,
+    val lightness: Double = Double.NaN,
+    val colorfulness: Doubl
