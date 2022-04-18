45d7c286199516baefdab6c6fa3e476b000db2c8
VioletVivian <100173231+VioletVivian@users.noreply.github.com>
2022-04-18 21:06:09 +0000

    iommu/amd: Fix hotplug with iommu=pt

--- app/src/main/java/cn/chitanda/music/ui/monet/theme/DynamicMonetColor.kt
+++ app/src/main/java/cn/chitanda/music/ui/monet/theme/DynamicMonetColor.kt
@@ -3,4 +3,11 @@ package cn.chitanda.music.ui.monet.theme
 import cn.chitanda.music.ui.monet.colors.Color
 import cn.chitanda.music.ui.monet.colors.Lch
 import cn.chitanda.music.ui.monet.colors.Oklab.Companion.toOklab
-import cn.chitanda.music.ui.m
+import cn.chitanda.music.ui.monet.colors.OklabGamut
+import cn.chitanda.music.ui.monet.colors.OklabGamut.clipToLinearSrgb
+import cn.chitanda.music.ui.monet.colors.Oklch
+import cn.chitanda.music.ui.monet.colors.Oklch.Companion.toOklch
+
+class DynamicMonetColor(
+    targets: MonetColor,
+    seedColor
