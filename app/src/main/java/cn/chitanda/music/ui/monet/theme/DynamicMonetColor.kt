037bf08a6e3e3ced15464cb771740f36a16bde0f
VioletVivian <100173231+VioletVivian@users.noreply.github.com>
2022-04-28 16:27:00 +0000

    Version bump to 1.4.0

--- app/src/main/java/cn/chitanda/music/ui/monet/theme/DynamicMonetColor.kt
+++ app/src/main/java/cn/chitanda/music/ui/monet/theme/DynamicMonetColor.kt
@@ -67,4 +67,9 @@ class DynamicMonetColor(
         val L = target.L
         // Allow colorless gray and low-chroma colors by clamping.
         // To preserve chroma ratios, scale chroma by the reference (cn.chitanda.music.A-1 / N-1).
-        val scaleC = if (reference.C == 0.0
+        val scaleC = if (reference.C == 0.0) {
+            // Zero reference C won't have chroma anyway, so use 0 to avoid a divide-by-zero
+            0.0
+        } else {
+            // Non-zero reference C = possible chroma scale
+            (seed.C.coerceIn(0.0, reference.C
