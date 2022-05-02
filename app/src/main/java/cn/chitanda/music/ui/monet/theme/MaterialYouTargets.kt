e7f25f8acfcdb7ee4b0af5adf1d0e1cb68444aee
VioletVivian <100173231+VioletVivian@users.noreply.github.com>
2022-05-02 12:41:56 +0000

    add .DS_Store

--- app/src/main/java/cn/chitanda/music/ui/monet/theme/MaterialYouTargets.kt
+++ app/src/main/java/cn/chitanda/music/ui/monet/theme/MaterialYouTargets.kt
@@ -1,3 +1,12 @@
 package cn.chitanda.music.ui.monet.theme
 
-import cn.chitanda.music.
+import cn.chitanda.music.ui.monet.colors.Color
+import cn.chitanda.music.ui.monet.colors.Oklch
+
+/*
+ * Default target colors, conforming to Material You standards.
+ *
+ * Derived from AOSP and Pixel defaults.
+ */
+class MaterialYouTargets(
+    private val chromaFactor: Double = 1.0,
