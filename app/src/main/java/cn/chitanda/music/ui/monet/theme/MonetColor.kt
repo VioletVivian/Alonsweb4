42d6f8be62983f49bbcc0b0f710296d4816d7d1a
VioletVivian <100173231+VioletVivian@users.noreply.github.com>
2022-05-14 17:21:37 +0000

    Tab police

--- app/src/main/java/cn/chitanda/music/ui/monet/theme/MonetColor.kt
+++ app/src/main/java/cn/chitanda/music/ui/monet/theme/MonetColor.kt
@@ -2,3 +2,12 @@ package cn.chitanda.music.ui.monet.theme
 
 import cn.chitanda.music.ui.monet.colors.Color
 
+typealias ColorSwatch = Map<Int, Color>
+
+abstract class MonetColor {
+    abstract val neutral1: ColorSwatch
+    abstract val neutral2: ColorSwatch
+
+    abstract val accent1: ColorSwatch
+    abstract val accent2: ColorSwatch
+    abstract val accent3: ColorSwa
