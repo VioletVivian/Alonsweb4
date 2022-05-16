0209a752a614b9fd8a9390b1be89a7313f8297bd
VioletVivian <100173231+VioletVivian@users.noreply.github.com>
2022-05-16 11:10:07 +0000

    Update french translation.

--- app/src/main/java/cn/chitanda/music/ui/monet/theme/SystemMonetColor.kt
+++ app/src/main/java/cn/chitanda/music/ui/monet/theme/SystemMonetColor.kt
@@ -7,4 +7,11 @@ import cn.chitanda.music.ui.monet.colors.Srgb
 @RequiresApi(31)
 class SystemMonetColor(private val context: Context) : MonetColor() {
     override val accent1 = getSwatch(ACCENT1_RES)
-    override val accent2 = getSwatch(ACC
+    override val accent2 = getSwatch(ACCENT2_RES)
+    override val accent3 = getSwatch(ACCENT3_RES)
+
+    override val neutral1 = getSwatch(NEUTRAL1_RES)
+    override val neutral2 = getSwatch(NEUTRAL2_RES)
+
+    private fun getSwatch(ids: Map<Int, Int>) = ids.map {
+        it.key to Srgb(context.g
