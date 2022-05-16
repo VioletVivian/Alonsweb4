8d90e2147d605051b3b5d7eb5641a60fdc304227
VioletVivian <100173231+VioletVivian@users.noreply.github.com>
2022-05-16 10:23:12 +0000

    General

--- app/src/main/java/cn/chitanda/music/ui/monet/theme/SystemMonetColor.kt
+++ app/src/main/java/cn/chitanda/music/ui/monet/theme/SystemMonetColor.kt
@@ -1,4 +1,10 @@
 package cn.chitanda.music.ui.monet.theme
 
 import android.content.Context
-impor
+import androidx.annotation.RequiresApi
+import cn.chitanda.music.ui.monet.colors.Srgb
+
+@RequiresApi(31)
+class SystemMonetColor(private val context: Context) : MonetColor() {
+    override val accent1 = getSwatch(ACCENT1_RES)
+    override val accent2 = getSwatch(ACC
