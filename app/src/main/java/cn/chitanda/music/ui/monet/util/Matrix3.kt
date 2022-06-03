968d133366fecaffd71b1997677b01c8552320af
VioletVivian <100173231+VioletVivian@users.noreply.github.com>
2022-06-03 19:31:12 +0000

    fix table name

--- app/src/main/java/cn/chitanda/music/ui/monet/util/Matrix3.kt
+++ app/src/main/java/cn/chitanda/music/ui/monet/util/Matrix3.kt
@@ -3,3 +3,14 @@ package cn.chitanda.music.ui.monet.util
 import kotlin.jvm.JvmInline
 
 @JvmInline
+value class Matrix3(
+    val values: DoubleArray,
+) {
+    constructor(
+        n1: Double, n2: Double, n3: Double,
+        n4: Double, n5: Double, n6: Double,
+        n7: Double, n8: Double, n9: Double,
+    ) : this(doubleArrayOf(
+        n1, n2, n3,
+        n4, n5, n6,
+       
