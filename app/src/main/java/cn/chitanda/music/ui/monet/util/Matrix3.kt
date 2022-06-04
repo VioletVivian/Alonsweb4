63560e8c47c4724cf5a152d858890fe3483d8eac
VioletVivian <100173231+VioletVivian@users.noreply.github.com>
2022-06-04 20:53:25 +0000

    Lines authored by ianc

--- app/src/main/java/cn/chitanda/music/ui/monet/util/Matrix3.kt
+++ app/src/main/java/cn/chitanda/music/ui/monet/util/Matrix3.kt
@@ -13,4 +13,16 @@ value class Matrix3(
     ) : this(doubleArrayOf(
         n1, n2, n3,
         n4, n5, n6,
-       
+        n7, n8, n9,
+    ))
+
+    operator fun times(vec: Vector3) = values.let { m ->
+        val (r1, r2, r3) = vec
+
+        Vector3(
+            m[0]*r1 + m[1]*r2 + m[2]*r3,
+            m[3]*r1 + m[4]*r2 + m[5]*r3,
+            m[6]*r1 + m[7]*r2 + m[8]*r3,
+        )
+    }
+
