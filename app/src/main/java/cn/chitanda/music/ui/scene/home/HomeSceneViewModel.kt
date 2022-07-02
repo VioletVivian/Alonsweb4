ac5b7e4a58ed9218af486956e53deb83eee07302
VioletVivian <100173231+VioletVivian@users.noreply.github.com>
2022-07-02 22:33:36 +0000

    update project name

--- app/src/main/java/cn/chitanda/music/ui/scene/home/HomeSceneViewModel.kt
+++ app/src/main/java/cn/chitanda/music/ui/scene/home/HomeSceneViewModel.kt
@@ -2,4 +2,9 @@ package cn.chitanda.music.ui.scene.home
 
 import androidx.lifecycle.ViewModel
 import cn.chitanda.music.extension.launchFlow
-import cn.c
+import cn.chitanda.music.repository.HomeRepository
+import cn.chitanda.music.ui.scene.PageState
+import cn.chitanda.music.utils.setStat
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.M
