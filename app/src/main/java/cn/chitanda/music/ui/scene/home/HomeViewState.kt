bb9c910c0037a7a3926fc37ad4ba00824caa0c2e
VioletVivian <100173231+VioletVivian@users.noreply.github.com>
2022-07-07 20:27:31 +0000

    New translations Resources.resx (Slovak)

--- app/src/main/java/cn/chitanda/music/ui/scene/home/HomeViewState.kt
+++ app/src/main/java/cn/chitanda/music/ui/scene/home/HomeViewState.kt
@@ -1,3 +1,11 @@
 package cn.chitanda.music.ui.scene.home
 
-import cn.chitanda.music.http.bean.HomeBa
+import cn.chitanda.music.http.bean.HomeBanner
+import cn.chitanda.music.http.bean.HomeData
+import cn.chitanda.music.http.bean.HomeRoundIcon
+import cn.chitanda.music.http.bean.MLogExtInfo
+import cn.chitanda.music.ui.scene.PageState
+
+/**
+ * @author: Chen
+ * @createTime: 2022/1/5 15:1
