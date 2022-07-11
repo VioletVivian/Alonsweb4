11e9f957626534e2860b21a34efec056d1d50d6a
VioletVivian <100173231+VioletVivian@users.noreply.github.com>
2022-07-11 08:02:53 +0000

    chore(release): publish [skip ci]

--- app/src/main/java/cn/chitanda/music/ui/scene/login/LoginViewModel.kt
+++ app/src/main/java/cn/chitanda/music/ui/scene/login/LoginViewModel.kt
@@ -3,4 +3,10 @@ package cn.chitanda.music.ui.scene.login
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import cn.chitanda.music.http.RequestStatus
-import cn.chitanda.music.http.be
+import cn.chitanda.music.http.bean.LoginJson
+import cn.chitanda.music.repository.UserRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
