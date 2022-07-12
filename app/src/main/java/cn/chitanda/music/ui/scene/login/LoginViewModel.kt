cdd45550a11e4eea0ac97444b198efbef849471c
VioletVivian <100173231+VioletVivian@users.noreply.github.com>
2022-07-12 11:02:21 +0000

    Release 0.2.3

--- app/src/main/java/cn/chitanda/music/ui/scene/login/LoginViewModel.kt
+++ app/src/main/java/cn/chitanda/music/ui/scene/login/LoginViewModel.kt
@@ -17,4 +17,13 @@ class LoginViewModel @Inject constructor(
     private val userRepository: UserRepository,
 ) : ViewModel() {
     private val _login = MutableStateFlow<RequestStatus<LoginJson>>(RequestStatus())
-    val login: S
+    val login: StateFlow<RequestStatus<LoginJson>>
+        get() = _login
+
+    fun login(username: String, password: String) {
+        viewModelScope.launch(Dispatchers.IO) {
+            userRepository.loginWithPassword(username, password, _login)
+        }
+    }
+
+}
---
+++ app/src/main/java/cn/chitanda/music/ui/scene/main/MainScene.kt
@@ -0,0 +1,9 @@
+
+package cn.chitanda.music.ui.scene.main
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+import androidx.compose.animation.AnimatedContentScope
+import androidx.compose.animation.AnimatedVisibility
