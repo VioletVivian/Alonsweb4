722e5edc4d343b5eeb8ce01736d40eb2a4977b22
VioletVivian <100173231+VioletVivian@users.noreply.github.com>
2022-07-17 17:26:37 +0000

    added new line

--- app/src/main/java/cn/chitanda/music/ui/scene/other/ThemeScene.kt
+++ app/src/main/java/cn/chitanda/music/ui/scene/other/ThemeScene.kt
@@ -1,1 +1,8 @@
-package cn.chitanda.music.ui.sc
+package cn.chitanda.music.ui.scene.other
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.
