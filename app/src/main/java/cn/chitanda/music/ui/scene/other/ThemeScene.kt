1b16a5e9afdf79485953e2da359e822364977a2c
VioletVivian <100173231+VioletVivian@users.noreply.github.com>
2022-07-17 22:50:53 +0000

    Version bump for npm

--- app/src/main/java/cn/chitanda/music/ui/scene/other/ThemeScene.kt
+++ app/src/main/java/cn/chitanda/music/ui/scene/other/ThemeScene.kt
@@ -5,4 +5,8 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.p
