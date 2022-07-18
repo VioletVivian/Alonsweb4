9551202c0698d2a8e9cf47b8d1ce32ed9db60f86
VioletVivian <100173231+VioletVivian@users.noreply.github.com>
2022-07-18 07:45:44 +0000

    Set up travis

--- app/src/main/java/cn/chitanda/music/ui/scene/other/ThemeScene.kt
+++ app/src/main/java/cn/chitanda/music/ui/scene/other/ThemeScene.kt
@@ -9,4 +9,10 @@ import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.p
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBars
+import androidx.compose.foundation.layout.windowInsetsTopHeight
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Slider
+import and
