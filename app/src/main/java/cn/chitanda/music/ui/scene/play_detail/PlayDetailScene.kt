afc3a5e1d47cfa05971bf99bd728e0bef03a8c9e
VioletVivian <100173231+VioletVivian@users.noreply.github.com>
2022-08-20 06:12:38 +0000

    pac

--- app/src/main/java/cn/chitanda/music/ui/scene/play_detail/PlayDetailScene.kt
+++ app/src/main/java/cn/chitanda/music/ui/scene/play_detail/PlayDetailScene.kt
@@ -5,4 +5,8 @@ import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.animation.core.AnimationVector1D
 import androidx.compose.animation.core.FastOutSlowInEasing
 import androidx.compose.animation.core.LinearEasing
-i
+import androidx.compose.animation.core.LinearOutSlowInEasing
+import androidx.compose.animation.core.RepeatMode
+import androidx.compose.animation.core.TwoWayConverter
+import androidx.compose.animation.core.animateFloat
+import androidx.compose.animation.core.inf
