fb52cc8f9c5abb3728efd5eaf156112b3dd70265
VioletVivian <100173231+VioletVivian@users.noreply.github.com>
2022-09-05 21:56:09 +0000

    fixing javadoc

--- app/src/main/java/cn/chitanda/music/ui/scene/splash/SplashScene.kt
+++ app/src/main/java/cn/chitanda/music/ui/scene/splash/SplashScene.kt
@@ -2,4 +2,10 @@ package cn.chitanda.music.ui.scene.splash
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
-import android
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+impor
