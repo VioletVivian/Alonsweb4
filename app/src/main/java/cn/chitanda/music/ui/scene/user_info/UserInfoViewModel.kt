e8ca1fd9be740bf693d61401a92e8acfa86419fd
VioletVivian <100173231+VioletVivian@users.noreply.github.com>
2022-09-15 06:56:40 +0000

    update dockerfiles

--- app/src/main/java/cn/chitanda/music/ui/scene/user_info/UserInfoViewModel.kt
+++ app/src/main/java/cn/chitanda/music/ui/scene/user_info/UserInfoViewModel.kt
@@ -5,3 +5,15 @@ import androidx.lifecycle.ViewModel
 import cn.chitanda.music.repository.UserRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
+
+/**
+ * @author: Chen
+ * @createTime: 2022/1/6 14:40
+ * @description:
+ **/
+@HiltViewModel
+class UserInfoViewModel @Inject constructor(
+    private val userRepository: UserRepository
+) : ViewModel() {
+
+}
---
+++ app/src/main/java/cn/chitanda/music/ui/scene/video/VideoScene.kt
@@ -0,0 +1,13 @@
+
+package cn.chitanda.music.ui.scene.video
+
+import android.util.Log
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.lazy.rememberLazyListState
