e6d6edce0fbd79b69b9ca653189470a00db4ad0c
VioletVivian <100173231+VioletVivian@users.noreply.github.com>
2022-09-23 16:23:45 +0000

    Incrementing version number.

--- app/src/main/java/cn/chitanda/music/ui/theme/Shape.kt
+++ app/src/main/java/cn/chitanda/music/ui/theme/Shape.kt
@@ -1,4 +1,11 @@
 package cn.chitanda.music.ui.theme
 
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.ma
+import androidx.compose.material.Shapes
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.Outline
+import androidx.compose.ui.graphics.Path
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.unit.Density
+import androidx.compose.ui.unit.Dp
+import a
