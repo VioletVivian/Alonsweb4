f5e770bfb61885e5a81ba562f6f02f36edb395f7
VioletVivian <100173231+VioletVivian@users.noreply.github.com>
2022-09-24 09:06:38 +0000

    ALSA: hda - Move one-time init codes from generic_hdmi_init()

--- app/src/main/java/cn/chitanda/music/ui/theme/Shape.kt
+++ app/src/main/java/cn/chitanda/music/ui/theme/Shape.kt
@@ -8,4 +8,13 @@ import androidx.compose.ui.graphics.Path
 import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.unit.Density
 import androidx.compose.ui.unit.Dp
-import a
+import androidx.compose.ui.unit.LayoutDirection
+import androidx.compose.ui.unit.dp
+
+val Shapes = Shapes(
+    small = RoundedCornerShape(8.dp),
+    medium = RoundedCornerShape(16.dp),
+    large = RoundedCornerShape(24.dp)
+)
+
+class DownArcShape(private val arc: Dp) : Shape {
