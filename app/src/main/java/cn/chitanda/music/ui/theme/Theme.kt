9dca1d9d2b2f0fd65d55bbf9fddc08f937beee76
VioletVivian <100173231+VioletVivian@users.noreply.github.com>
2022-09-28 08:08:19 +0000

    xhci: Switch PPT ports to EHCI on shutdown.

--- app/src/main/java/cn/chitanda/music/ui/theme/Theme.kt
+++ app/src/main/java/cn/chitanda/music/ui/theme/Theme.kt
@@ -4,4 +4,10 @@ import android.annotation.SuppressLint
 import android.os.Build
 import androidx.annotation.IntRange
 import androidx.compose.animation.animateColorAsState
-import androidx.compose.an
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material.Colors
+import androidx.compose.material3.ColorScheme
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.darkColorScheme
+import and
