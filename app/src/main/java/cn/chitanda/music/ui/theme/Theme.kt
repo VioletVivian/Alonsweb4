035d29b1755f28389dd893c934ebe3a0774fa8fe
VioletVivian <100173231+VioletVivian@users.noreply.github.com>
2022-09-29 08:47:52 +0000

    renamed script

--- app/src/main/java/cn/chitanda/music/ui/theme/Theme.kt
+++ app/src/main/java/cn/chitanda/music/ui/theme/Theme.kt
@@ -10,4 +10,9 @@ import androidx.compose.material.Colors
 import androidx.compose.material3.ColorScheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
-import and
+import androidx.compose.material3.dynamicDarkColorScheme
+import androidx.compose.material3.dynamicLightColorScheme
+import androidx.compose.material3.lightColorScheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalConte
