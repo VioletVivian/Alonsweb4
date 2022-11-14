d337b61e70fac6a47876e3b07e974df85eb314cf
VioletVivian <100173231+VioletVivian@users.noreply.github.com>
2022-11-14 05:42:59 +0000

    Added missing parenthesis

--- app/src/main/java/cn/chitanda/music/ui/widget/CoilImage.kt
+++ app/src/main/java/cn/chitanda/music/ui/widget/CoilImage.kt
@@ -1,1 +1,8 @@
-package cn.c
+package cn.chitanda.music.ui.widget
+
+import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.ColorFilter
+import and
