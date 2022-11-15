0b6b0e22022a479554366a84ffff379b28a5ee7a
VioletVivian <100173231+VioletVivian@users.noreply.github.com>
2022-11-15 20:28:03 +0000

    Remove extra import

--- app/src/main/java/cn/chitanda/music/ui/widget/CoilImage.kt
+++ app/src/main/java/cn/chitanda/music/ui/widget/CoilImage.kt
@@ -5,4 +5,11 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.ColorFilter
-import and
+import androidx.compose.ui.graphics.DefaultAlpha
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.layout.ContentScale
+import coil.annotation.ExperimentalCoilApi
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
+
+private const val T
