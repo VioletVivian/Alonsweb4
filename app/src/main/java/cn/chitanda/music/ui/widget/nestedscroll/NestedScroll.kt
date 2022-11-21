e11127320e0b7eb42f58e1e8808a68fee78cca70
VioletVivian <100173231+VioletVivian@users.noreply.github.com>
2022-11-21 11:52:04 +0000

    Minor optimisation

--- app/src/main/java/cn/chitanda/music/ui/widget/nestedscroll/NestedScroll.kt
+++ app/src/main/java/cn/chitanda/music/ui/widget/nestedscroll/NestedScroll.kt
@@ -2,4 +2,10 @@ package cn.chitanda.music.ui.widget.nestedscroll
 
 import androidx.compose.animation.core.Easing
 import androidx.compose.animation.core.FastOutSlowInEasing
-import 
+import androidx.compose.animation.core.animate
+import androidx.compose.animation.core.tween
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.Stable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+im
