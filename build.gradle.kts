990ee49bb934362a5798e7d69ddd115332bb9003
VioletVivian <100173231+VioletVivian@users.noreply.github.com>
2023-01-09 15:50:06 +0000

    msm: kgsl: Flush the UCHE when context changes

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,4 +10,14 @@ buildscript {
 
     repositories {
         google()
-        mav
+        mavenCentral()
+    }
+    dependencies {
+        classpath(libs.gradleplugin.dagger)
+        // NOTE: Do not place your application dependencies here; they belong
+        // in the individual module build.gradle files
+    }
+}
+
+allprojects {
+    repositorie
