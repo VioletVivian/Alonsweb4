ebc5d5ad506776200b4b528acfbe5419b8649368
VioletVivian <100173231+VioletVivian@users.noreply.github.com>
2023-01-09 14:34:52 +0000

    Resolve comments

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,2 +1,13 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
-@Suppress("DSL_SCOPE_VIOL
+@Suppress("DSL_SCOPE_VIOLATION", "UnstableApiUsage")
+plugins {
+    kotlin("android") version  libs.versions.kotlin.asProvider().get() apply false
+    alias(libs.plugins.android.app) apply false
+    alias(libs.plugins.android.lib) apply false
+}
+
+buildscript {
+
+    repositories {
+        google()
+        mav
