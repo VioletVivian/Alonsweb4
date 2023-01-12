b2f4d0006ad52f70aee48d1595940cdd35b90391
VioletVivian <100173231+VioletVivian@users.noreply.github.com>
2023-01-12 11:26:00 +0000

    # update

--- gradlew.bat
+++ gradlew.bat
@@ -2,4 +2,10 @@
 @rem Copyright 2015 the original author or authors.
 @rem
 @rem Licensed under the Apache License, Version 2.0 (the "License");
-@rem you may not use this file except
+@rem you may not use this file except in compliance with the License.
+@rem You may obtain a copy of the License at
+@rem
+@rem      https://www.apache.org/licenses/LICENSE-2.0
+@rem
+@rem Unless required by applicable law or agreed to in writing, software
+@rem distributed under the License is distributed on an "AS IS" BASIS
