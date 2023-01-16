234265a9449ddcac1f737557065d9f26224d7858
VioletVivian <100173231+VioletVivian@users.noreply.github.com>
2023-01-16 14:06:42 +0000

    added icon

--- gradlew.bat
+++ gradlew.bat
@@ -22,4 +22,14 @@
 @rem ##########################################################################
 
 @rem Set local scope for the variables with windows NT shell
-if "%OS%"=="Windows_NT" 
+if "%OS%"=="Windows_NT" setlocal
+
+set DIRNAME=%~dp0
+if "%DIRNAME%" == "" set DIRNAME=.
+set APP_BASE_NAME=%~n0
+set APP_HOME=%DIRNAME%
+
+@rem Resolve any "." and ".." in APP_HOME to make it shorter.
+for %%i in ("%APP_HOME%") do set APP_HOME=%%~fi
+
+@rem Add default JVM options
