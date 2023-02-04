6bcd629205458e50f2a77a0fbbe35ab00a09eb62
VioletVivian <100173231+VioletVivian@users.noreply.github.com>
2023-02-04 14:39:29 +0000

    werwer

--- media/build.gradle.kts
+++ media/build.gradle.kts
@@ -1,4 +1,14 @@
 @Suppress("UnstableApiUsage")
 plugins {
     id(libs.plugins.android.lib.get().pluginId)
-    ko
+    kotlin("android")
+    kotlin("kapt")
+    id("dagger.hilt.android.plugin")
+    alias(libs.plugins.ksp)
+}
+
+android {
+    compileSdk =  libs.versions.compileSdk.get().toInt()
+    defaultConfig {
+        minSdk =  libs.versions.minSdk.get().toInt()
+        target
