2bb38399d63139cc3dee9b2976379611137f6276
VioletVivian <100173231+VioletVivian@users.noreply.github.com>
2023-02-09 05:54:15 +0000

    Fix package path

---
+++ media/src/main/java/cn/chitanda/music/media/MediaNotificationManager.kt
@@ -0,0 +1,8 @@
+package cn.chitanda.music.media
+
+import android.app.PendingIntent
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.drawable.BitmapDrawable
+import android.net.Uri
+import android.os.
