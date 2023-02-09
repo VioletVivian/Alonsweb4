44c954fe480c8df69cc7a9989373751213701cc0
VioletVivian <100173231+VioletVivian@users.noreply.github.com>
2023-02-09 08:03:03 +0000

    more integration tests

--- media/src/main/java/cn/chitanda/music/media/MediaNotificationManager.kt
+++ media/src/main/java/cn/chitanda/music/media/MediaNotificationManager.kt
@@ -5,4 +5,9 @@ import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.drawable.BitmapDrawable
 import android.net.Uri
-import android.os.
+import android.os.Build
+import android.support.v4.media.session.MediaControllerCompat
+import android.support.v4.media.session.MediaSessionCompat
+import androidx.core.app.NotificationChannelCompat
+import androidx.core.app.NotificationManagerCompat
+import cn.chitanda.music.media.extensions.displayS
